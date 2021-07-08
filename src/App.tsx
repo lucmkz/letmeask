fe5d950a52f864ca7c194d704a0589e744928359
Lucas Santos <l.duarte.mmk@gmail.com>
2021-07-08 14:52:23 -0300

    create newroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Home } from "./pages/home";
+// import { Home } from "./pages/Home";
 
+import { NewRoom } from "./pages/NewRoom";
 import "./services/firebase";
 import "./styles/global.scss";
 
 function App() {
   return (
-    <Home />
+    // <Home />
+    <NewRoom />
   );
 }
 
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,30 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logo from "../assets/images/logo.svg";
+
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração" />
+        <strong>Create your Q&amp;A room live</strong>
+        <p>answer your audience in real-time</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logo} alt="letmeask" />
+          <h2>Create New Room</h2>
+          <form>
+            <input type="text" placeholder="Room`s name" />
+            <Button type="submit">Create room</Button>
+          </form>
+          <p>
+            Do you want to join in a existent Room? <a href="#">Click here</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
