22d98ee8c2975fb4b688310aa8027220481f2980
Lucas Santos <l.duarte.mmk@gmail.com>
2021-07-13 09:11:16 -0300

    auth google

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,12 +7,22 @@ import googleIconImage from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
+import { auth, firebase } from "../services/firebase";
+
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push("/room/new");
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+    });
+
+    // history.push("/room/new");
   }
+
+  function signIn() {}
 
   return (
     <div id="page-auth">
@@ -24,7 +34,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logo} alt="letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImage} alt="Icon Logo" />
             Create your room with Google
           </button>
